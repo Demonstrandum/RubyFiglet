ced623f4c075951be42f2c5f87581c43491304e2
Demonstrandum <knutsen@jetspace.co>
2017-09-23 20:05:59 +0100

    Update README and next version

--- lib/ruby_figlet.rb
+++ lib/ruby_figlet.rb
@@ -1,7 +1,7 @@
 Dir["#{File.dirname __FILE__}/ruby_figlet/*.rb"].each { |f| require f }
 
 module RubyFiglet
-  VERSIONS = { :major => 0, :minor => 5, :tiny => 0 }
+  VERSIONS = { :major => 0, :minor => 5, :tiny => 1 }
 
   def self.version *args
     VERSIONS.flatten.select.with_index { |val, i| i.odd? }.join '.'
