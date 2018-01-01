1c9b470e99f6c86cde4e62266fa2eb473a533841
Demonstrandum <knutsen@jetspace.co>
2018-01-01 23:47:55 +0000

    Bump version

--- lib/ruby_figlet.rb
+++ lib/ruby_figlet.rb
@@ -1,9 +1,10 @@
 Dir["#{File.dirname __FILE__}/ruby_figlet/*.rb"].each { |f| require f }
 
+# Main namespace for all RubyFIGlet classes and methods.
 module RubyFiglet
-  VERSIONS = { :major => 0, :minor => 5, :tiny => 1 }
+  VERSIONS = { :major => 0, :minor => 6, :tiny => 1 }.freeze
 
   def self.version *args
-    VERSIONS.flatten.select.with_index { |val, i| i.odd? }.join '.'
+    VERSIONS.flatten.select.with_index { |_, i| i.odd? }.join '.'
   end
 end
